7261f1eb70edb5a75095c218ed6066c923506a90
lemondouble <lemondouble2@gmail.com>
2023-06-08 08:46:00 +0900

    fix : SSR 문제 해결을 위해 NoSSR 컴포넌트로 감싸줌

--- src/components/LemonSSO.tsx
+++ src/components/LemonSSO.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Divider, Paper, Stack, Typography } from '@mui/material';
+import { Avatar, Button, Divider, NoSsr, Paper, Stack, Typography } from '@mui/material';
 import GoogleSvgIcon from './icons/GoogleSvgIcon.tsx';
 
 export interface LemonSSOProps {
@@ -12,24 +12,26 @@ export function LemonSSO(props: LemonSSOProps) {
   }
 
   return (
-    <Paper
-      sx={{
-        width: '400px',
-        padding: '32px',
-      }}
-    >
-      <Stack direction="column" spacing={2} justifyContent="center" alignItems="center">
-        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-          <Avatar alt="Site Logo" src="/lemon-logo.webp" />
-          <Typography variant="h6">Lemon Toolbox</Typography>
+    <NoSsr>
+      <Paper
+        sx={{
+          width: '400px',
+          padding: '32px',
+        }}
+      >
+        <Stack direction="column" spacing={2} justifyContent="center" alignItems="center">
+          <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+            <Avatar alt="Site Logo" src="/lemon-logo.webp" />
+            <Typography variant="h6">Lemon Toolbox</Typography>
+          </Stack>
+          <Typography variant="body1">계속 하려면 로그인 해 주세요.</Typography>
+          <Typography variant="caption">로그인 정보는 유저 식별 이외에 아무 곳에도 사용되지 않습니다.</Typography>
+          <Divider sx={{ width: '100%' }} />
+          <Button variant="outlined" startIcon={<GoogleSvgIcon />} href={googleLoginUrl.toString()}>
+            Google Id로 계속하기
+          </Button>
         </Stack>
-        <Typography variant="body1">계속 하려면 로그인 해 주세요.</Typography>
-        <Typography variant="caption">로그인 정보는 유저 식별 이외에 아무 곳에도 사용되지 않습니다.</Typography>
-        <Divider sx={{ width: '100%' }} />
-        <Button variant="outlined" startIcon={<GoogleSvgIcon />} href={googleLoginUrl.toString()}>
-          Google Id로 계속하기
-        </Button>
-      </Stack>
-    </Paper>
+      </Paper>
+    </NoSsr>
   );
 }
